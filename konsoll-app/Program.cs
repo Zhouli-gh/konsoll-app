296a83aaebd7903ae13ae068d0654ad334c21d87
zhouli <zhoulikvaal@gmail.com>
2025-05-07 13:14:35 +0200

    Oppgave 1: konsoll-app

---
+++ konsoll-app/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace konsoll_app;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Hva heter du?");
+       var input = Console.ReadLine();
+       Console.WriteLine($"Velkommen {input}!");
+    }
+}
